4a9707968397a429db427fbccc7d513e8296ff42
ymom5 <maincraftlol11@gmail.com>
2024-09-15 21:01:42 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Введите первое число:"))
+num2 = int(input("Введите второе число:"))
+if num1 < num2:
+    print(num1)
+if num2 < num1:
+    print(num2)
---
+++ 2.py
@@ -0,0 +1,5 @@
+num = int(input("Введите целое число: "))
+if 1000 <= num <= 9999:
+    print ("YES")
+else: 
+    print ("NO")
